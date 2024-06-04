90ef4e0c9dce8aafab8a82c0cba7d9a5812f1fd3
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-05 00:53:39 +0530

    add those fetched data into the blog cards

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function fetchRandomNews(){
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apikey}`;
     const response=await fetch(apiUrl)
     const data= await response.json()
+    return data.articles;
 
 
   }catch(error){
@@ -16,14 +17,35 @@ async function fetchRandomNews(){
   }
 }
 
+function displayBlogs(articles) {
+  blogContainer.innerHTML =""
+  articles.forEach((article)=>{
+    const blogCard=document.createElement("div");
+    blogCard.classList.add("blog-card")
+    const img=document.createElement("img")
+    img.src= article.urlToImage
+    img.alt= article.title
+    const title= document.createElement("h2")
+    title.textContent=article.title
+    const description=document.createElement("p")
+    description.textContent=article.description
+
+    blogCard.appendChild(img)
+    blogCard.appendChild(title)
+    blogCard.appendChild(description)
+    blogContainer.appendChild(blogCard)
+
+  })
+  
+}
 
 (async()=>{
   try{
     const articles=await fetchRandomNews()
-    console.log(articles)
+    displayBlogs(articles)
 
   }catch(error){
     console.error("Error fetching random news",error)
   }
 
-})
+})();
