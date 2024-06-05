04b3022ca6e416393b465f1de9e272ef4d7d9289
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-05 23:53:59 +0530

    implemented the search functionality and  did some error handling

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const apikey = "f42cb48cce5a4e959007b4d57d0f1201";
 
 const blogContainer = document.getElementById("blog-container");
+const searchField=document.getElementById("search-input")
+const searchButton=document.getElementById("search-button")
+
+searchButton.addEventListener("click",async ()=>{
+  const query=searchField.value.trim();
+  if(query!==""){
+    try{
+      const articles= await fetchNewsQuery(query)
+      displayBlogs(articles)
+
+    }catch(error){
+      console.log("Error fetching  news by query",error)
+    }
+  }
+
+})
+
+async function fetchNewsQuery(query){
+  try {
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=20&apiKey=${apikey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error("Error fetching news", error);
+    return [];
+  }
+
+}
 
 async function fetchRandomNews() {
   try {
@@ -20,21 +49,21 @@ function displayBlogs(articles) {
     const blogCard = document.createElement("div");
     blogCard.classList.add("blog-card");
     const img = document.createElement("img");
-    img.src = article.urlToImage;
+    img.src = article.urlToImage || "error imaage.webp";
     img.alt = article.title;
     const title = document.createElement("h2");
-    // const truncatedTitle =
-    //   article.title.length > 30
-    //     ? article.title.slice(0, 30) + "...."
-    //     : article.title;
-    title.textContent=article.title;
+    const truncatedTitle =
+      article.title.length > 30
+        ? article.title.slice(0, 30) + "...."
+        : article.title;
+    title.textContent=truncatedTitle;
     const description = document.createElement("p");
-    // const truncatedDesc=
-    //   article.description.length > 120
-    //     ? article.description.slice(0, 120) + "...."
-    //     : article.description;
+    const truncatedDesc =
+      article.description && article.description.length > 100
+        ? article.description.slice(0, 120) + "...."
+        : article.description;
 
-    description.textContent = article.description;
+    description.textContent = truncatedDesc;
 
     blogCard.appendChild(img);
     blogCard.appendChild(title);
