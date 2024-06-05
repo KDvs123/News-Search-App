831f422990f7f73ec72ba826648d216f21dd954e
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-05 23:34:01 +0530

    news search app js implementation with some error handlings

--- script.js
+++ script.js
@@ -2,50 +2,55 @@ const apikey = "f42cb48cce5a4e959007b4d57d0f1201";
 
 const blogContainer = document.getElementById("blog-container");
 
-async function fetchRandomNews(){
-  try{
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apikey}`;
-    const response=await fetch(apiUrl)
-    const data= await response.json()
+async function fetchRandomNews() {
+  try {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&apiKey=${apikey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
     return data.articles;
-
-
-  }catch(error){
-    console.error("Error fetching news",error)
-    return []
-
+  } catch (error) {
+    console.error("Error fetching news", error);
+    return [];
   }
 }
 
 function displayBlogs(articles) {
-  blogContainer.innerHTML =""
-  articles.forEach((article)=>{
-    const blogCard=document.createElement("div");
-    blogCard.classList.add("blog-card")
-    const img=document.createElement("img")
-    img.src= article.urlToImage
-    img.alt= article.title
-    const title= document.createElement("h2")
-    title.textContent=article.title
-    const description=document.createElement("p")
-    description.textContent=article.description
+  blogContainer.innerHTML = "";
+  articles.forEach((article) => {
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
+    const img = document.createElement("img");
+    img.src = article.urlToImage;
+    img.alt = article.title;
+    const title = document.createElement("h2");
+    // const truncatedTitle =
+    //   article.title.length > 30
+    //     ? article.title.slice(0, 30) + "...."
+    //     : article.title;
+    title.textContent=article.title;
+    const description = document.createElement("p");
+    // const truncatedDesc=
+    //   article.description.length > 120
+    //     ? article.description.slice(0, 120) + "...."
+    //     : article.description;
 
-    blogCard.appendChild(img)
-    blogCard.appendChild(title)
-    blogCard.appendChild(description)
-    blogContainer.appendChild(blogCard)
+    description.textContent = article.description;
 
-  })
-  
+    blogCard.appendChild(img);
+    blogCard.appendChild(title);
+    blogCard.appendChild(description);
+    blogCard.addEventListener('click',()=>{
+      window.open(article.url, "_blank");
+    } );
+    blogContainer.appendChild(blogCard);
+  });
 }
 
-(async()=>{
-  try{
-    const articles=await fetchRandomNews()
-    displayBlogs(articles)
-
-  }catch(error){
-    console.error("Error fetching random news",error)
+(async () => {
+  try {
+    const articles = await fetchRandomNews();
+    displayBlogs(articles);
+  } catch (error) {
+    console.error("Error fetching random news", error);
   }
-
 })();
